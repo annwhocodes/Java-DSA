9bf4b320aa8c501da020af5b2dcc0f45024facd4
annwhocodes <ananyeah98@gmail.com>
2024-12-28 16:30:49 +0530

    Assignment Operators

--- Operators/AssignmentOperators.java
+++ Operators/AssignmentOperators.java
@@ -1,3 +1,10 @@
-public class AssignmentOperators {
-    public static void main (String args[])
+public class AssignmentOperators  {
+    public static void main (String args[]){
+        int A = 10;
+        int B = 5;
+        A+=5;
+        B-=5;
+        System.out.println(A);
+        System.out.println(B);   
+    }
 }
