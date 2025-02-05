9e615efe367b32995708984f31c1ffe31dfd4dd3
annwhocodes <ananyeah98@gmail.com>
2025-02-05 23:54:59 +0530

    Loops

---
+++ Loops/whileLoop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class whileLoop {
+    public static void main (String args[]){
+        int count = 0;
+        while (count<=10){
+            System.out.println("Hello World");
+            count++;
+        }
+        System.out.println("Printed Hello World 10 times");
+    }
+}
