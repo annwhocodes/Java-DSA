770527aa48ea49cbf22bdb1c0504b5251e33f398
annwhocodes <ananyeah98@gmail.com>
2025-02-08 22:43:46 +0530

    New Program

---
+++ Loops/count1to10.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class count1to10 {
+    public static void main(String args[]){
+        int counter = 1;
+        while(counter<=10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
---
+++ Loops/count1ton.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class count1ton {
+    public static void main(String agrs[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+        int count = 1;
+        while(count<=n){
+            System.out.println(count);
+            count++;
+        
+        }
+
+    }
+}
