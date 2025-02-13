ffce3fa1397c47340f9be410163aeef115820a08
annwhocodes <ananyeah98@gmail.com>
2025-02-13 23:03:29 +0530

    New Files

---
+++ Loops/reversenumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class reversenumber {
+    public static void main (String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int rev = 0;
+        for (int i=0 ; i<=0)
+    
+    }
+}
